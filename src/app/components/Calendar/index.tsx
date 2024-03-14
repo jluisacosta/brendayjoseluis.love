2aabed98e5beb61dc82885526a7187bfbd8a2d0c
José Luis Acosta <jluis.acosta@outlook.com>
2024-03-14 13:54:18 -0600

    fix(layout): Adapt layout for multiple screen sizes (rotation)

--- src/app/components/Calendar/index.tsx
+++ src/app/components/Calendar/index.tsx
@@ -20,7 +20,7 @@ const Heart = () => (
     0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
     c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z'
     />
-    <text x='7' y='20' className={kosugiMaru.className} font-size='16' fill='white'>
+    <text x='7' y='20' className={kosugiMaru.className} fontSize='16' fill='white'>
       {HIGHLIGHT}
     </text>
   </svg>
