e79ce370cbc9e1ebf1d71c017a9baaa8a286574a
José Luis Acosta <jluis.acosta@outlook.com>
2024-03-15 22:07:45 -0600

    feat(lookandfeel): slides updates, style tweaks and animations

--- src/app/components/Carousel/images.ts
+++ src/app/components/Carousel/images.ts
@@ -1,4 +1,4 @@
 export const CAROUSEL_1 = ['1.jpg', '2.jpg'];
 export const CAROUSEL_2 = ['3.jpg', '4.jpg'];
 export const CAROUSEL_3 = ['5.jpg', '6.jpg'];
-export const CAROUSEL_ALL = ['1.jpg', '5.jpg', '2.jpg', '3.jpg', '6.jpg', '4.jpg'];
+export const CAROUSEL_ALL = ['1.jpg', '3.jpg', '6.jpg', '4.jpg', '5.jpg', '2.jpg'];
--- src/app/components/NamesHeading/index.tsx
+++ src/app/components/NamesHeading/index.tsx
@@ -7,7 +7,9 @@ const NamesHeading = () => {
       <h3 className={`${styles.namesHeading} ${greatVibes.className}`}>
         <span>Brenda &amp;José Luis</span>
       </h3>
-      <h4 className={`${styles.location} ${encodeC.className}`}>San Luis Potosí, México</h4>
+      <h4 className={`${styles.location} ${encodeC.className}`}>
+        <span>San Luis Potosí, México</span>
+      </h4>
     </>
   );
 };
