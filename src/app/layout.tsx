2c175782c7c20c816b03d6a1a5d06ee40e1cee14
José Luis Acosta <jluis.acosta@outlook.com>
2024-06-17 15:04:40 -0600

    feat(metadata): Set Open Graph meta tags for link preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,28 @@ import type { Metadata } from 'next';
 import './styles/globals.scss';
 
 export const metadata: Metadata = {
+  description: 'Estás cordialmente invitado a nuestra boda.',
+  openGraph: {
+    description: 'Estás cordialmente invitado a nuestra boda.',
+    images: [
+      {
+        width: 1600,
+        height: 1067,
+        url: 'https://nuestraboda.love/Brenda-y-JoseLuis/wp-content/uploads/sites/2599/2024/05/Final.jpg',
+        type: 'image/jpeg',
+      },
+    ],
+    locale: 'es_ES',
+    siteName: 'Brenda & José Luis',
+    title: 'Brenda & José Luis - Nuestra Boda',
+    type: 'website',
+    url: 'https://brendayjoseluis.love',
+  },
+  robots: 'noindex, nofollow',
   title: 'Brenda & José Luis',
-  description: 'Save The Date',
+  twitter: {
+    card: 'summary_large_image',
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
